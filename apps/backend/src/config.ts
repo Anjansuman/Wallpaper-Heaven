f27899c271a51dae8604473359b9aa5464b56372
ankitR3 <ankitraj828401@gmail.com>
2026-07-28 11:02:50 +0530

    fix: correct missing/typo'd env variable exports in config.ts

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,10 +1,10 @@
 
 // Backend specific
 export const FRONTEND_URL = process.env.FRONTEND_URL;
-export const JWT_SECRET = process.env.JWT_SECRET || "13e23iydv2uy!@ywejhvw";
+export const JWT_SECRET = process.env.JWT_SECRET;
 
 
 // AWS S3 specific
-export const region = process.env.region;
-export const accessKeyId = process.env.ACCESS_KEY_ID;
-export const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+export const region = process.env.AWS_REGION;
+export const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+export const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
